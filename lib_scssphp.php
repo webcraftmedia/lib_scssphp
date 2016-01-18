bac779a7b9ef053b8253d4d76c594b03b19157aa
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-18 03:28:44 +0100

    submodule scssphp

--- lib_scssphp.php
+++ lib_scssphp.php
@@ -1,11 +1,10 @@
 <?php
 namespace LIB;
-class lib_scssphp extends \LIB\lib_jscss{
+class lib_scssphp extends \LIB\lib_php{
     public static function get_class(){
         return self::class;}
     public static function php_autoload(){
-        require_once dirname(__FILE__).'/lib/Michelf/Markdown.inc.php';
-        require_once dirname(__FILE__).'/lib/Michelf/MarkdownExtra.inc.php';
+        require_once dirname(__FILE__).'/lib/scss.inc.php';
         return true;}
     public static function version(){
         return 'https://github.com/leafo/scssphp/';}
