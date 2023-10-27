3d829591ea59eb1fbc92844b88659d921484f12a
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-10-27 16:20:10 +0200

    updated submodule

--- lib_scssphp.php
+++ lib_scssphp.php
@@ -4,8 +4,13 @@ class lib_scssphp extends \LIB\lib_php{
     public static function get_class(){
         return self::class;}
     public static function php_autoload(){
-        require_once dirname(__FILE__).'/lib/scss.inc.php';
+        //require_once dirname(__FILE__).'/lib/scss.inc.php';
+        require_once dirname(__FILE__).'/lib/src/Logger/LoggerInterface.php';
+        require_once dirname(__FILE__).'/lib/src/Logger/StreamLogger.php';
+        require_once dirname(__FILE__).'/lib/src/Type.php';
+        require_once dirname(__FILE__).'/lib/src/OutputStyle.php';
+        require_once dirname(__FILE__).'/lib/src/Compiler.php';
         return true;}
     public static function version(){
-        return 'https://github.com/leafo/scssphp/';}
+        return 'https://github.com/scssphp/scssphp';}
 }
